6181846e08cf1d113621a0db5dcb995c3a7e0a81
James Marwood <james.marwood@gmail.com>
2026-04-04 14:47:32 +0100

    Add logo and favicon

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "James Marwood's Public Notes",
+    pageTitle: "Public Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
