41613aed04ff1ad192df2ba832e2a50c3876abca
MarwoodChap <james@thatmarwoodchap.com>
2026-08-09 16:29:03 +0100

    Update quartz.layout.ts
    Updated sidebar behaviour

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,7 +47,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+   Component.Explorer(explorerOptions),
   ],
   right: [
     Component.Graph(),
@@ -71,7 +71,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer(explorerOptions),
   ],
   right: [],
 }
